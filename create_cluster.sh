99bb20b4ea8e36a8046d32624c6d165e56d8ca93
brunopecampos <brunopecampos@protonmail.com>
2023-01-05 16:23:55 -0300

    Added network policies to project

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-minikube start -p k8s-node-cassandra -n 3
+minikube start -p k8s-node-cassandra -n 3 --cni=cilium --memory=4096
 minikube profile k8s-node-cassandra
 ./nfs-storage/install_csi_driver.sh
 sudo ./nfs-storage/run_nfs_server.sh
-./ingress/ingress_install.sh
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install --generate-name --values=./frontend/values.yaml bitnami/nginx
 kubectl apply -f ./nfs-storage/nfs_sc.yaml
@@ -14,6 +13,7 @@ kubectl apply -f ./frontend/frontend_service.yaml
 kubectl apply -f ./backend/backend_deploy.yaml
 kubectl apply -f ./backend/backend_service.yaml
 kubectl apply -f ./database/database_service.yaml
-kubectl apply -f ./ingress/ingress.yaml
 kubectl apply -f ./database/database_statefulset.yaml
+#./ingress/ingress_install.sh
+#kubectl apply -f ./ingress/ingress.yaml
 # sudo echo "192.168.X.X" >> /etc/hosts
