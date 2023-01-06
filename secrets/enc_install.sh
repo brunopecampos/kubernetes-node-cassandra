fda645a030326a3c0a96bdc3adab66d312ef6118
brunopecampos <brunopecampos@protonmail.com>
2023-01-06 10:00:29 -0300

    Fixed secret encryption

--- secrets/enc_install.sh
+++ secrets/enc_install.sh
@@ -4,13 +4,16 @@
 ## First create /etc/kubernetes/enc dir with minikube ssh, than
 minikube cp ./enc.yaml /etc/kubernetes/enc/enc.yaml
 # edit the api server pod manifest according to the tutorial
-# to test out 
-minikube cp ./hexdump /hexdump
-minikube cp ./etcdctl /etcdctl
+minikube ssh -- sudo vi /etc/kubernetes/manifests/kube-apiserver.yaml
+# apply changes to api-server
+minikube ssh -- sudo mv /etc/kubernetes/manifests/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver2.yaml
 
+# to test out 
+minikube cp ./etcdctl /etcdctl
+minikube ssh -- sudo chmod +x /etcdctl
 # execute:
 # create a secret:
-# kubectl create secret generic secret1 -n default --from-literal=mykey=mydata
+kubectl create secret generic secret1 -n default --from-literal=mykey=mydata
 # and retrieve its information from etcd
-#sudo ETCDCTL_API=3 /etcdctl --cacert=/var/lib/minikube/certs/etcd/ca.crt --cert=/var/lib/minikube/certs/etcd/server.crt --key=/var/lib/minikube/certs/etcd/server.key get /registry/secrets/default/secret1
+minikube ssh -- sudo ETCDCTL_API=3 /etcdctl --cacert=/var/lib/minikube/certs/etcd/ca.crt --cert=/var/lib/minikube/certs/etcd/server.crt --key=/var/lib/minikube/certs/etcd/server.key get /registry/secrets/default/secret1
 
